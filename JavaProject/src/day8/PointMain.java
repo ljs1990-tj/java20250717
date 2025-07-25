3f2b94a971b1442560f19c7bfa47b66b7a6cd038
tj-bu-708-강사PC <tj-bu-708-PC@DESKTOP-9596UBR>
2025-07-25 12:44:20 +0900

    2024.07.25 상속 연습문제 4

--- JavaProject/src/day8/PointMain.java
+++ JavaProject/src/day8/PointMain.java
@@ -19,15 +19,24 @@ public class PointMain {
 //		cp.setColor("RED");
 //		System.out.println(cp.toString() + "입니다.");
 
-		Point3D p = new Point3D(1, 2, 3); // 1,2,3은 각각 x, y, z축의 값.
+		// 3.
+//		Point3D p = new Point3D(1, 2, 3); // 1,2,3은 각각 x, y, z축의 값.
+//		System.out.println(p.toString() + "입니다.");
+//		p.moveUp(); // z 축으로 위쪽 이동
+//		System.out.println(p.toString() + "입니다.");
+//		p.moveDown(); // z 축으로 아래쪽 이동
+//		p.move(10, 10); // x, y 축으로 이동
+//		System.out.println(p.toString() + "입니다.");
+//		p.move(100, 200, 300); // x, y, z축으로 이동
+//		System.out.println(p.toString() + "입니다.");
+
+		PositivePoint p = new PositivePoint();
+		p.move(10, 10);
 		System.out.println(p.toString() + "입니다.");
-		p.moveUp(); // z 축으로 위쪽 이동
+		p.move(-5, 5); // 객체 p는 음수 공간으로 이동되지 않음
 		System.out.println(p.toString() + "입니다.");
-		p.moveDown(); // z 축으로 아래쪽 이동
-		p.move(10, 10); // x, y 축으로 이동
-		System.out.println(p.toString() + "입니다.");
-		p.move(100, 200, 300); // x, y, z축으로 이동
-		System.out.println(p.toString() + "입니다.");
+		PositivePoint p2 = new PositivePoint(-10, -10);
+		System.out.println(p2.toString() + "입니다.");
 
 	}
 
--- JavaProject/src/day8/PositivePoint.java
+++ JavaProject/src/day8/PositivePoint.java
@@ -5,4 +5,24 @@ public class PositivePoint extends Point{
 	PositivePoint(){
 		super(0, 0);
 	}
+	PositivePoint(int x, int y){
+		super(x, y);
+		if(x < 0 || y < 0) {
+			super.move(0, 0);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "(" + getX() + "," + getY() + ") 의 점";
+	}
+	
+	@Override
+	protected void move(int x, int y) {
+		// TODO Auto-generated method stub
+		if(x >= 0 && y >= 0) {
+			super.move(x, y);
+		}
+	}
 }
