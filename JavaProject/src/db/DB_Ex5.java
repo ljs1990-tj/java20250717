b3135cba3b5dee9f7ab3b885835b899b2d953f4f
JSLee <ljs1990@gmail.com>
2025-08-13 17:11:46 +0900

    2025.08.13 Oracle 연결

---
+++ JavaProject/src/db/DB_Ex5.java
@@ -0,0 +1,13 @@
+package db;
+
+public class DB_Ex5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 2. 아이디, 비밀번호, 이름, 성별, 나이를 입력받은 후
+		// TBL_USER에 저장. (CDATETIME는 현재시간 저장)
+		// 단, 아이디 입력 시에 이미 테이블에서 사용하는 아이디를 입력할 경우
+		// '이미 사용중인 아이디 입니다' 출력 후 다시 입력받도록 함
+	}
+
+}
