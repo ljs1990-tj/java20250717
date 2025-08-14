d52238bad0e6d35cf38881c3cea8dc5f9c35f91b
JSLee <ljs1990@gmail.com>
2025-08-14 09:53:55 +0900

    2025.08.14 유저관리 프로그램 문제 예시

--- JavaProject/src/db/DB_Ex6.java
+++ JavaProject/src/db/DB_Ex6.java
@@ -1,13 +1,36 @@
 package db;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class DB_Ex6 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		// 로그인
 		// 아이디, 비밀번호 입력받아서
 		// 두개와 일치하는 레코드가 있으면 '로그인 성공!'
 		// 없으면 '로그인 실패!' 출력
+		
+		Scanner s = new Scanner(System.in);
+		DBClass db = new DBClass();
+		
+		System.out.print("아이디 : ");
+		String id = s.next();
+		System.out.print("비밀번호 : ");
+		String pwd = s.next();
+		
+		String query = "SELECT * FROM TBL_USER "
+						+ "WHERE USERID = '" + id + "' AND PASSWORD = '" + pwd + "'";
+		
+		ResultSet rs = db.stmt.executeQuery(query);
+		if(rs.next()) {
+			System.out.println("로그인 성공!");
+		} else {
+			System.out.println("로그인 실패!");
+		}
+		
+		
 	}
-
 }
